615c4ce62d9aa024a3d73d27b1a515630674ccf4
K-Praveen22 <praveen22daya@gmail.com>
2024-10-08 22:15:52 +0530

    Create 12_XNOR_gate_tb.v

---
+++ GATES/12_XNOR_gate_tb.v
@@ -0,0 +1,14 @@
+module xnor_gate_tb;
+   reg a,b;
+   wire y;
+  xnor_gate n1(a,b,y);
+  initial begin
+    $monitor("a=%d,b=%d,y=%d",a,b,y);
+    a=0;b=0;#2;
+    a=0;b=1;#2;
+    a=1;b=0;#2;
+    a=1;b=1;#2
+    $finish;
+  end
+endmodule
+    
