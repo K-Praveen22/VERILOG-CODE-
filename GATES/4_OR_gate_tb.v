8c3dfab357fc04633fbefc8fb30183d8d90b18e3
K-Praveen22 <praveen22daya@gmail.com>
2024-10-08 21:57:08 +0530

    Create 4_OR_gate_tb.v

---
+++ GATES/4_OR_gate_tb.v
@@ -0,0 +1,14 @@
+module or_gate_tb;
+   reg a,b;
+   wire y;
+  or_gate o1(a,b,y);
+  initial begin
+    $monitor("a=%d,b=%d,y=%d",a,b,y);
+    a=0;b=0;#2;
+    a=0;b=1;#2;
+    a=1;b=0;#2;
+    a=1;b=1;#2
+    $finish;
+  end
+endmodule
+    
