b10db73eabbfb32a1472bb25af5d4be89d809a77
K-Praveen22 <praveen22daya@gmail.com>
2024-10-08 22:04:48 +0530

    Create 8_NOR_gate_tb.v

---
+++ GATES/8_NOR_gate_tb.v
@@ -0,0 +1,15 @@
+module or_gate_tb;
+   reg a,b;
+   wire y;
+  nor_gate n1(a,b,y);
+  initial begin
+    $monitor("a=%d,b=%d,y=%d",a,b,y);
+    a=0;b=0;#2;
+    a=0;b=1;#2;
+    a=1;b=0;#2;
+    a=1;b=1;#2
+    $finish;
+  end
+endmodule
+    
+    
