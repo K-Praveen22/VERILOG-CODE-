3723fe383c87620a229413e0ca09c0f6fde2371e
K-Praveen22 <praveen22daya@gmail.com>
2024-11-03 23:26:21 +0530

    Create 8:3 BINARY ENCODER.v

---
+++ 8:3 BINARY ENCODER.v
@@ -0,0 +1,9 @@
+// Code your design here
+module binary_encoder(
+  input [7:0] d,
+  output [3:0] y); 
+  assign y[2]=d[4]|d[5]|d[6]|d[7];
+  assign y[1]=d[2]|d[3]|d[6]|d[7];
+  assign y[0]=d[1]|d[3]|d[5]|d[7];
+endmodule
+  
