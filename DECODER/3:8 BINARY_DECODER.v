45ac278a81af08db47402b42389838ef2dc8e876
K-Praveen22 <praveen22daya@gmail.com>
2024-11-05 15:09:49 +0530

    Update 3:8 BINARY_DECODER.v

--- DECODER/3:8 BINARY_DECODER.v
+++ DECODER/3:8 BINARY_DECODER.v
@@ -3,7 +3,8 @@ module binary_decoder(
   input[2:0]d,
   output reg [7:0]y);
   always @(d)begin
-    case (d)
+    y=0;
+    case(d)
       3'b000:y[0]=1'b1;
       3'b001:y[1]=1'b1;
       3'b010:y[2]=1'b1;
@@ -12,6 +13,8 @@ module binary_decoder(
       3'b101:y[5]=1'b1;
       3'b110:y[6]=1'b1;
       3'b111:y[7]=1'b1;
-    endcase
-  end
-endmodule
+     default:y=0;
+     endcase
+      end
+    endmodule
+       
