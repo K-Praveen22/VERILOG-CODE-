b1c440ed5d5e80c66a1ddc386fb58205798dd674
K-Praveen22 <praveen22daya@gmail.com>
2024-11-06 16:50:57 +0530

    Create ALTERNATE_ZEROS_ONEStb.v

---
+++ FSM CODES/ALTERNATE_ZEROS_ONEStb.v
@@ -0,0 +1,40 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+  reg clk, rst, a;
+  wire y;
+  
+  alt_zeros_ones alter(clk, rst, a, y);
+  
+  initial clk = 0;
+  always #10 clk=~clk;
+  
+  initial begin
+    a = 0;
+    #6 rst = 0;
+    #7 rst = 1;
+    
+    #14 a=1;
+    #14 a=1;
+    #14 a=0;
+    #14 a=1;
+    #14 a=0;
+    #14 a=1;
+    #14 a=1;
+    #14 a=0;
+    #14 a=1;
+    #14 a=0;
+    #14 a=1;
+    #14 a=0;
+    #14 a=1;
+    #14 a=1;
+    #30 $finish;
+  end
+  
+  initial begin 
+    $dumpfile("waves.vcd");
+    $dumpvars;
+  end
+  
+endmodule
+    
