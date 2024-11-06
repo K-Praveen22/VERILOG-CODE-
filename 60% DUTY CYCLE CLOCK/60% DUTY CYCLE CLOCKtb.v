599b769178dd743e5da448e61519b74e129b6d5f
K-Praveen22 <praveen22daya@gmail.com>
2024-11-06 21:25:57 +0530

    Create 60% DUTY CYCLE CLOCKtb.v

---
+++ 60% DUTY CYCLE CLOCK/60% DUTY CYCLE CLOCKtb.v
@@ -0,0 +1,22 @@
+// Code your testbench here
+// or browse Examples
+module tb;
+  reg in_clk,rst;
+  wire out_clk;
+  
+  duty_cycle duty(in_clk,rst,out_clk);
+  
+  initial in_clk = 0;
+  always #2 in_clk = ~in_clk;
+  
+  initial begin
+    rst = 1;
+    #10 rst =0;
+    #200 $finish;
+  end
+  
+  initial begin
+    $dumpfile("waves.vcd");
+    $dumpvars;
+  end
+endmodule
